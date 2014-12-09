9d67a464dc0b7e2b8382a0378d5cead1dba39d85
Delmer Reed <delmer814@yahoo.com>
2014-12-09 07:21:05 -0800

    initial setup

---
+++ models/user.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var bcrypt = require("bcrypt");
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    email: DataTypes.STRING,
+    password_digest: DataTypes.STRING
+  }, {
+    instanceMethods: {
+      checkPassword: function (password) {
+        return bcrypt.compareSync(password, this.password_digest);
+      }
+    },
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      },
+      encryptPassword: function (password) {
+        var salt = bcrypt.genSaltSync(13);
+        var hash = bcrypt.hashSync(password, salt);
+        return hash;
+      },
+      createSecure: function (email, password, error, success) {
+        var hash = this.encryptPassword(password);
+        this.create({
+          email: email,
+          password_digest: hash
+        })
+        .then(function (user) {
+                    console.log("Whatt?")
+          success({message: 'Account Created'});
+        })
+        .fail(function (err) {
+          console.log("Whatt?")
+          console.log(arguments)
+          console.log(err)
+          error({message: "something went wrong"});
+        });
+      },
+      authenticate: function (email, password, err, success) {
+        this.find({
+          where: {
+            email: email
+          }
+        }).then(function (user) {
+          if (user.checkPassword(password)) {
+            success();
+          }
+        }).error(function (err) {
+          error({message: "Invalid data"});
+        })
+      }
+    }
+  });
+
+  return user;
+};
