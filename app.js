e5f638193acb59fde55eb9c748a15766b9347366
Delmer Reed <delmer814@yahoo.com>
2014-12-09 16:58:49 -0800

    chage

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var express = require("express"),
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+/*
+  What is the session?
+
+  It is the object that lives in our app
+    and records relevant info about users
+    who are signed in
+*/
 app.use(session( {
   secret: 'thisismysecretkey',
   name: 'chocolate chip',
@@ -15,21 +23,28 @@ app.use(session( {
   maxage: 3600000
   })
 );
-app.get("/sign_up", function (req, res) {
-  res.render("users/sign_up");
-});
 
 // get passport started
 app.use(passport.initialize());
 app.use(passport.session());
 
-// prepare our serialize functions
+/*
+SERIALizING
+Turns relevant user data into a string to be 
+  stored as a cookie
+*/
 passport.serializeUser(function(user, done){
   console.log("SERIALIZED JUST RAN!");
 
   done(null, user.id);
 });
 
+/*
+DeSERIALizing
+
+Taking a string and turns into an object
+  using the relevant data stored in the session
+*/
 passport.deserializeUser(function(id, done){
   console.log("DESERIALIZED JUST RAN!");
   db.user.find({
@@ -45,21 +60,31 @@ passport.deserializeUser(function(id, done){
     });
 });
 
+// WHEN SOMEONE WANTS THE SIGNUP PAGE
+app.get("/sign_up", function (req, res) {
+  res.render("users/sign_up");
+});
+
+// WHEN SOMEONE  SUBMITS A SIGNUP PAGE
 app.post("/users", function (req, res) {
   console.log("POST /users");
   var newUser = req.body.user;
   console.log("New User:", newUser);
+  // CREATE a user and secure their password
   db.user.createSecure(newUser.email, newUser.password, 
     function () {
+      // if a user fails to create make them signup again
       res.redirect("/sign_up");
     },
     function (err, user) {
+      // when successfully created log the user in
+      // req.login is given by the passport
       req.login(user, function(){
+        // after login redirect show page
         console.log("Id: ", user.id)
         res.redirect('/users/' + user.id);
       });
-    }
-  )
+    })
 });
 
 
@@ -74,10 +99,13 @@ app.get("/users/:id", function (req, res) {
     })
 });
 
+
+// When someone wants the login page
 app.get("/login", function (req, res) {
   res.render("users/login");
 });
 
+// Authenticating a user
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login'
@@ -86,6 +114,8 @@ app.post('/login', passport.authenticate('local', {
 
 app.get("/", function (req, res) {
   console.log(req.user)
+  // req.user is the user currently logged in
+
   if (req.user) {
     res.render("site/index", {user: req.user});
   } else {
@@ -94,6 +124,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/logout", function (req, res) {
+  // log out
   req.logout();
   res.redirect("/");
 });
--- console.js
+++ console.js
@@ -1,5 +1,5 @@
 var repl = require('repl');
 var db = require('./models');
 
-var newRepl = repl.start("node console>");
+var newRepl = repl.start("node console> ");
 newRepl.context.db = db;
